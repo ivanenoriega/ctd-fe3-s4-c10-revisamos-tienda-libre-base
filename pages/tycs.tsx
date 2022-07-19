84e2bcab51c4d4d7ecc65b3900d5c25f786b8779
Alexis <alexis.lazzuri@intive-fdv.com>
2022-07-19 13:57:08 -0300

    create code base

--- pages/tycs.tsx
+++ pages/tycs.tsx
@@ -4,23 +4,11 @@ import { TyC, TyCsAPIResponse } from "../types";
 import styles from "../styles/TYC.module.css";
 import Head from "next/head";
 
-// Por ahora estamos utilizando data mockeada, pero
-// debemos reemplazar esto por información proveniente de la
-// API
-export const data: TyCsAPIResponse = {
-  version: "3 de julio, 2022",
-  tycs: [
-    {
-      id: 1,
-      title: "General",
-      description: `Tienda Libre es una compañía que ofrece servicios vinculados principalmente al comercio electrónico. 
-                    Los servicios están diseñados para formar un ecosistema que permita a las personas vender, 
-                    comprar, pagar, enviar productos y realizar otras actividades comerciales con tecnología aplicada.`,
-    },
-  ],
+type IProps = {
+  data: TyCsAPIResponse;
 };
 
-const TerminosYCondiciones: NextPage = () => {
+const TerminosYCondiciones: NextPage<IProps> = ({ data }) => {
   if (!data) return null;
 
   const { version, tycs } = data;
@@ -48,7 +36,16 @@ const TerminosYCondiciones: NextPage = () => {
   );
 };
 
-// Aquí debemos agregar el método para obtener la información
-// de la API
+export async function getStaticProps() {
+  const baseUrl = "http://localhost:3000/"; // Cambiar por la url del proyecto una vez deployada la API
+
+  const response = await fetch(`${baseUrl}/api/tycs`);
+
+  const data = await response.json();
+
+  return {
+    props: { data },
+  };
+}
 
 export default TerminosYCondiciones;
